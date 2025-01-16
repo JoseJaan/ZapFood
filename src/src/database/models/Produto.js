027c7b3153af298f174c0aae11fb94ec2fe6197c
José Acerbi <joseacerbialmeidaneto@hotmail.com>
2025-01-16 13:31:13 -0300

    :sparkles: feat: criado model produto

---
+++ src/src/database/models/Produto.js
@@ -0,0 +1,56 @@
+const dotenv = require('dotenv');
+const { Sequelize } = require('sequelize');
+const path = require('path');
+dotenv.config();
+const database = require(path.resolve("config", "database.js"));
+
+const Produto = database.define('produto', {
+    idProduto: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4, 
+    },
+    precoProduto: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    descricaoProduto: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        validate: {
+            len: [15, 150]
+        }
+    },
+    nomeProduto: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+        validate: {
+            len: [10, 60]
+        }
+    },
+    Loja_idLoja: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+            model: 'loja', //referencia a tabela loja
+            key: 'idLoja'
+        }
+    },
+    visibilidade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            isIn: [[0, 1]] 
+        },
+        defaultValue: 1,
+    },
+    categoriaProduto: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+        validate: {
+            len: [10, 60]
+        }
+    }
+});
+
+module.exports = Produto;
