1c4a32871eac1cb79d71f48c5df18af4ce219aaf
José Acerbi <joseacerbialmeidaneto@hotmail.com>
2025-01-22 20:53:14 -0300

    :sparkles: feat: integrado pagina da loja

--- src/src/controllers/LojaController.js
+++ src/src/controllers/LojaController.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const lojaService = require(path.resolve("src", "services", "Loja.js"));
+const produtoService = require(path.resolve("src", "services", "Produto.js"));
 class LojaController{
 
     static async detalharLoja(req,res){
@@ -17,6 +18,22 @@ class LojaController{
             console.error(error.message);
             return res.status(400).send("Erro ao obter loja");
         }
+    }
+
+    static async buscarLoja(req,res){
+        const lojaId = req.params.idLoja;
+
+        const loja = await lojaService.obterLoja(lojaId);
+        console.log("loja no controller",loja)
+        const produtos = await produtoService.listarProdutos(lojaId); 
+        console.log("produtos no controller", produtos)
+        const produtosEmPromocao = produtos.filter(produto => produto.desconto > 0);
+        res.render('paginaDaEmpresa', {
+            loja: loja,
+            produtosEmPromocao: produtosEmPromocao,
+            todosProdutos: produtos,
+        });
+
     }
 
     static async paginaPrincipal(req,res){
--- src/src/routes/CRUD/Loja.js
+++ src/src/routes/CRUD/Loja.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require("path");
 const router = express.Router();
-const authMiddleware = require(path.resolve("src","middleware","lojaMiddleware.js"));
+const clienteMiddleware = require(path.resolve("src","middleware","clienteMiddleware.js"));
+const lojaMiddleware = require(path.resolve("src","middleware","lojaMiddleware.js"));
 const LojaController = require(path.resolve("src","controllers","LojaController.js"));
 
-router.get("/loja", authMiddleware,LojaController.detalharLoja);
-router.get("/loja/detalhar",authMiddleware,LojaController.obterLoja);
-router.get("/paginaPrincipalLoja",authMiddleware,LojaController.paginaPrincipal);
+router.get("/empresa/:idLoja",clienteMiddleware, LojaController.buscarLoja);
+router.get("/loja", lojaMiddleware, LojaController.detalharLoja);
+router.get("/loja/detalhar", clienteMiddleware, LojaController.obterLoja);
+router.get("/paginaPrincipalLoja", lojaMiddleware, LojaController.paginaPrincipal);
+
 
 module.exports = router;
